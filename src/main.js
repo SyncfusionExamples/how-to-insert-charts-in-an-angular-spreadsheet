969719f23a0b488b8e2d9c49dc586b6da8895d52
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2023-05-18 12:14:08 +0530

    Delete src directory

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import {registerLicense} from '@syncfusion/ej2-base';
-createApp(App).mount('#app')
-registerLicense("Your license key")
